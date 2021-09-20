4e44743803a8f3bfd3394acc67cd9088c9fd94ff
jyhoover <57460072+jyhoover@users.noreply.github.com>
2021-09-19 23:38:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"michaelc@thedataincubator.com\"\n\
+email = \"jiayang.hu@mg.thedataincubator.com.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
